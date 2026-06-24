cdcabd70460b26b45f6c2b05bc9bb819c081807a
MakisVarv <christomvarv@yahoo.com>
2026-06-24 20:09:13 +0300

    updates

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Booking, Menu
 
-admin.site.register(Booking)
+# Register your models here.
+from .models import Menu
+from .models import Booking
+
+
 admin.site.register(Menu)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,12 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import BookingForm
 from .models import Menu
+from django.core import serializers
+from .models import Booking
+from datetime import datetime
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -11,6 +16,13 @@ def home(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+def reservations(request):
+    date = request.GET.get("date", datetime.today().date())
+    bookings = Booking.objects.all()
+    booking_json = serializers.serialize("json", bookings)
+    return render(request, "bookings.html", {"bookings": booking_json})
 
 
 def book(request):
@@ -23,9 +35,11 @@ def book(request):
     return render(request, "book.html", context)
 
 
+# Add your code here to create new views
 def menu(request):
     menu_data = Menu.objects.all()
-    return render(request, "menu.html", {"menu": menu_data})
+    main_data = {"menu": menu_data}
+    return render(request, "menu.html", {"menu": main_data})
 
 
 def display_menu_item(request, pk=None):
@@ -34,3 +48,31 @@ def display_menu_item(request, pk=None):
     else:
         menu_item = ""
     return render(request, "menu_item.html", {"menu_item": menu_item})
+
+
+@csrf_exempt
+def bookings(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        date = data["reservation_date"]
+        slot = data["reservation_slot"]
+
+        exists = (
+            Booking.objects.filter(reservation_date=date)
+            .filter(reservation_slot=slot)
+            .exists()
+        )
+        if exists:
+            return HttpResponse('{"error": 1}', content_type="application/json")
+
+        Booking.objects.create(
+            first_name=data["first_name"],
+            reservation_date=date,
+            reservation_slot=slot,
+        )
+
+    else:
+        date = request.GET.get("date", datetime.today().date())
+    bookings = Booking.objects.filter(reservation_date=date)
+    booking_json = serializers.serialize("json", bookings)
+    return HttpResponse(booking_json, content_type="application/json")
