9ba1406bbb5746f44e4bba097f99fb9c48837e03
MakisVarv <christomvarv@yahoo.com>
2026-06-25 15:44:27 +0300

    create api views for items and booking

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from restaurant import views
 
+router = DefaultRouter()
+router.register(r"tables", views.BookingViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("restaurant/booking/", include(router.urls)),
     path("", include("restaurant.urls")),
 ]
