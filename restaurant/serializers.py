704f812ba00bd5d86f4c9526424011c603eb2664
MakisVarv <christomvarv@yahoo.com>
2026-06-25 15:43:42 +0300

    change menu model to menuitem model

---
+++ restaurant/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from restaurant.models import Booking, MenuItem
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    description = serializers.CharField(source="menu_item_description")
+    stock = serializers.IntegerField(source="inventory")
+
+    class Meta:
+        model = MenuItem
+        fields = [
+            "id",
+            "name",
+            "price",
+            "stock",
+            "description",
+        ]
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = "__all__"
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from .forms import BookingForm
-from .models import Menu
+from .models import MenuItem
 from django.core import serializers
 from .models import Booking
 from datetime import datetime
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from rest_framework import viewsets
+from .serializers import BookingSerializer
+
+
+# Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
 
 
 # Create your views here.
@@ -35,19 +52,23 @@ def book(request):
     return render(request, "book.html", context)
 
 
-# Add your code here to create new views
 def menu(request):
-    menu_data = Menu.objects.all()
+    menu_data = MenuItem.objects.all()
     main_data = {"menu": menu_data}
     return render(request, "menu.html", {"menu": main_data})
 
 
 def display_menu_item(request, pk=None):
     if pk:
-        menu_item = Menu.objects.get(pk=pk)
+        menu_item = MenuItem.objects.get(pk=pk)
     else:
         menu_item = ""
     return render(request, "menu_item.html", {"menu_item": menu_item})
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
 
 
 @csrf_exempt
