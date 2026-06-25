398a9194675b1a610ff6e646342d7b45af886a1e
MakisVarv <christomvarv@yahoo.com>
2026-06-25 17:19:41 +0300

    clean

--- LittleLemonAPI/LittleLemonAPI/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from LittleLemonAPI.models import Category, MenuItem
-
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(MenuItem)
--- LittleLemonAPI/littlelemon/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("LittleLemonAPI.urls")),
-    path("api/", include("djoser.urls")),
-    path("", include("djoser.urls.authtoken")),
-]
