32959e93b28e6fa11488ba0caf3b6cb40222f9e0
MakisVarv <christomvarv@yahoo.com>
2026-06-30 17:43:54 +0300

    fixes

--- backend/littlelemon/settings.py
+++ backend/littlelemon/settings.py
@@ -15,6 +15,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 # Application definition
 
@@ -29,9 +33,11 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "djoser",
     "restaurant",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
